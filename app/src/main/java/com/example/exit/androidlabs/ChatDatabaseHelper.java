0838b99766190c352295a8c6f3e57f42a84260ca
EXIT2652 <shen0082@algonquinlive.com>
2017-11-08 17:24:39 -0500

    Lab 5, has not been finished yet. Committing for updating Android Studio 3.0.

--- app/src/main/java/com/example/exit/androidlabs/ChatDatabaseHelper.java
+++ app/src/main/java/com/example/exit/androidlabs/ChatDatabaseHelper.java
@@ -1,12 +1,14 @@
 package com.example.exit.androidlabs;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 /**
- * Created by EXIT on 10/16/2017.
+ * Created by Zhan Shen on 10/16/2017.
  */
 
 public class ChatDatabaseHelper extends SQLiteOpenHelper {
@@ -17,17 +19,14 @@ public class ChatDatabaseHelper extends SQLiteOpenHelper {
     public static final String TABLE_NAME = "entry";
     public static final String KEY_ID = "id";
     public static final String KEY_MESSAGE = "message";
+    private String TAG_SQL = ChatDatabaseHelper.class.getSimpleName();
 
-//    public static String[] MESSAGE_FIELDS = new String[] {
-//            KEY_ID,
-//            KEY_MESSAGE,
-//    };
+    private SQLiteDatabase database;
 
-
-    private static String SQL_CREATE_TABLE =
-            "CREATE TABLE" + TABLE_NAME + "("
-                    + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
-                    + KEY_MESSAGE + " TEXT" + ")";
+    private static final String SQL_CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " (" +
+            KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            KEY_MESSAGE + " TEXT" +
+            ")";
 
     public ChatDatabaseHelper (Context ctx) {
         super(ctx, DATABASE_NAME, null, VERSION_NUM);
@@ -35,17 +34,15 @@ public class ChatDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
         db.execSQL(SQL_CREATE_TABLE);
-        Log.i("ChatDatabaseHelper", "Calling onCreate");
+        Log.i(TAG_SQL, "Calling onCreate");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVer, int newVer) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
-
         onCreate(db);
-        Log.i("ChatDatabaseHelper","Calling onUpgrade, oldVersion=" + oldVer + "newVersion=" +newVer);
+        Log.i(TAG_SQL,"Calling onUpgrade, oldVersion=" + oldVer + "newVersion=" +newVer);
     }
 
     @Override
@@ -58,6 +55,31 @@ public class ChatDatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onOpen(SQLiteDatabase db) {
         Log.i("Database ", "onOpen was called");
+    }
+
+    public void openDatabase() {
+        database = this.getWritableDatabase();
+    }
+
+
+    public void insertEntry(String content) {
+        ContentValues values = new ContentValues();
+        values.put(KEY_MESSAGE, content);
+        database.insert(TABLE_NAME, null, values);
+    }
+
+    public void closeDatabase() {
+        if(database != null && database.isOpen()){
+            database.close();
+        }
+    }
+
+/*    public void deleteLastItem() {
+        getWritableDatabase().execSQL("DELETE FROM " + TABLE_NAME + " WHERE " + KEY_ID + " = (SELECT MAX(" + KEY_ID + ") FROM " + TABLE_NAME + ")");
+    }*/
+
+    public Cursor getRecords() {
+        return database.query(TABLE_NAME, null, null, null, null, null, null);
     }
 
 }
--- app/src/main/java/com/example/exit/androidlabs/ChatWindow.java
+++ app/src/main/java/com/example/exit/androidlabs/ChatWindow.java
@@ -24,87 +24,18 @@ import static com.example.exit.androidlabs.ChatDatabaseHelper.TABLE_NAME;
 
 public class ChatWindow extends Activity {
 
-    ListView listView;
-    EditText editText;
-    Button buttonSend;
-    ArrayList<String> chatArray;
-    String ACTIVITY_NAME = "ChatWindow";
+    private ChatDatabaseHelper chatDBHelper;
+    private String chatWindow = ChatWindow.class.getSimpleName();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat_window);
+    private ListView listView;
+    private EditText editText;
+    private Button buttonSend;
+    private ArrayList<String> chatArray;
+//    String ACTIVITY_NAME = "ChatWindow";
+    private ChatAdapter messageAdapter;
 
-        listView = (ListView)findViewById(R.id.listView);
-        editText = (EditText)findViewById(R.id.plain_text_input);
-        buttonSend = (Button)findViewById(R.id.buttonSend);
-        chatArray = new ArrayList<String>();
+    class ChatAdapter extends ArrayAdapter<String>{
 
-        //in this case, "this" is the ChatWindow, which is - A Context object
-        final ChatAdapter messageAdapter = new ChatAdapter(this);
-        listView.setAdapter(messageAdapter);
-
-        //Create a temporary ChatDatabaseHelper object, which then gets a writeable database
-        ChatDatabaseHelper chatDBHelper = new ChatDatabaseHelper(this);
-        final SQLiteDatabase db = chatDBHelper.getWritableDatabase();
-
-        //Click on Send button to send text
-        buttonSend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                chatArray.add(editText.getText().toString());
-                messageAdapter.notifyDataSetChanged();//this restarts the process of getCount()/getView()
-                editText.setText("");
-
-                ContentValues insertNewMessage = new ContentValues();
-                insertNewMessage.put(KEY_MESSAGE, editText.getText().toString());
-                db.insert(TABLE_NAME, "", insertNewMessage);
-            }
-        });
-
-        //Hit Enter key on keyboard to send text
-        editText.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View view, int keyCode, KeyEvent event) {
-                if (event.getAction() == KeyEvent.ACTION_DOWN){
-                    switch(keyCode) {
-                        case KeyEvent.KEYCODE_DPAD_CENTER:
-                        case KeyEvent.KEYCODE_ENTER:
-                            chatArray.add(editText.getText().toString());
-                            messageAdapter.notifyDataSetChanged();//this restarts the process of getCount()/getView()
-                            editText.setText("");
-                            return true;
-                        default:
-                            break;
-                    }
-                }
-                return false;
-            }
-        });
-
-
-        Cursor cursor = db.query(false, TABLE_NAME, new String[]{KEY_ID, KEY_MESSAGE},
-                null, null, null, null, null, null);
-
-        cursor.moveToFirst();
-        while(!cursor.isAfterLast() ) {
-            Log.i(ACTIVITY_NAME, "SQL MESSAGE:" + cursor.getString(cursor.getColumnIndex(ChatDatabaseHelper.KEY_MESSAGE)));
-
-            chatArray.add(cursor.getString(cursor.getColumnIndex(KEY_MESSAGE)));
-            cursor.moveToNext();
-        }
-        Log.i(ACTIVITY_NAME,"Cursor's column count = " + cursor.getColumnCount());
-
-        cursor.moveToFirst();
-        for(int i=0; i < cursor.getCount(); i++) {
-            Log.i(ACTIVITY_NAME,"The " + i + " row is " + cursor.getString(cursor.getColumnIndex(KEY_MESSAGE)));
-            cursor.moveToNext();
-        }
-
-
-    }
-
-    private class ChatAdapter extends ArrayAdapter<String>{
         public ChatAdapter(Context ctx){
             super(ctx, 0);
         }
@@ -133,29 +64,127 @@ public class ChatWindow extends Activity {
         }
     }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_window);
+
+        listView = (ListView) findViewById(R.id.listView);
+        editText = (EditText) findViewById(R.id.plain_text_input);
+        buttonSend = (Button) findViewById(R.id.buttonSend);
+        chatArray = new ArrayList<String>();
+
+        //in this case, "this" is the ChatWindow, which is - A Context object
+        messageAdapter = new ChatAdapter(this);
+        listView.setAdapter(messageAdapter);
+//        registerForContextMenu(listView);
+
+        //Create a temporary ChatDatabaseHelper object, which then gets a writeable database
+        chatDBHelper = new ChatDatabaseHelper(this);
+
+
+
+
+/*        final SQLiteDatabase db = chatDBHelper.getWritableDatabase();*/
+
+        //Click on Send button to send text
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (editText.getText().toString().equals("") == false) {
+                    chatArray.add(editText.getText().toString());
+                    messageAdapter.notifyDataSetChanged();//this restarts the process of getCount()/getView()
+
+                    chatDBHelper.insertEntry(editText.getText().toString());
+                    editText.setText("");
+                }
+            }
+        });
+
+        //Hit Enter key on keyboard to send text
+        editText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                    switch (keyCode) {
+                        case KeyEvent.KEYCODE_DPAD_CENTER:
+                        case KeyEvent.KEYCODE_ENTER:
+                            chatArray.add(editText.getText().toString());
+                            messageAdapter.notifyDataSetChanged();//this restarts the process of getCount()/getView()
+
+                            chatDBHelper.insertEntry(editText.getText().toString());
+                            editText.setText("");
+                            return true;
+                        default:
+                            break;
+                    }
+                }
+                return false;
+            }
+        });
+
+/*        Cursor cursor = chatDBHelper.getRecords();
+        if (cursor.moveToFirst()) {
+            while (!cursor.isAfterLast()) {
+                Log.i(chatWindow, "SQL MESSAGE:" + cursor.getString(cursor.getColumnIndex(ChatDatabaseHelper.KEY_MESSAGE)));
+
+                chatArray.add(cursor.getString(cursor.getColumnIndex(ChatDatabaseHelper.KEY_MESSAGE)));
+                cursor.moveToNext();
+            }
+            Log.i(chatWindow, "Cursor's column count = " + cursor.getColumnCount());
+
+            cursor.moveToFirst();
+            for (int i = 0; i < cursor.getColumnCount(); i++) {
+                Log.i(chatWindow, "The " + i + " row is " + cursor.getColumnName(i));
+            }
+        }*/
+    }
+
+    private void displaySQL() {
+        Cursor cursor = chatDBHelper.getRecords();
+        if (cursor.moveToFirst()) {
+            while (!cursor.isAfterLast()) {
+                Log.i(chatWindow, "SQL MESSAGE:" + cursor.getString(cursor.getColumnIndex(ChatDatabaseHelper.KEY_MESSAGE)));
+
+                chatArray.add(cursor.getString(cursor.getColumnIndex(ChatDatabaseHelper.KEY_MESSAGE)));
+                cursor.moveToNext();
+            }
+            Log.i(chatWindow, "Cursor's column count = " + cursor.getColumnCount());
+        }
+
+            for (int i = 0; i < cursor.getColumnCount(); i++) {
+                Log.i(chatWindow, "The " + i + " row is " + cursor.getColumnName(i));
+            }
+
+    }
+
     protected void onResume(){
         super.onResume();
-        Log.i(ACTIVITY_NAME, "In onResume()");
+        Log.i(chatWindow, "In onResume()");
     }
 
     protected void onStart(){
         super.onStart();
-        Log.i(ACTIVITY_NAME,"In onStart()");
+        chatDBHelper.openDatabase();
+        displaySQL();
+
+        Log.i(chatWindow,"In onStart()");
     }
 
     protected void onPause(){
         super.onPause();
-        Log.i(ACTIVITY_NAME,"In onPause()");
+        Log.i(chatWindow,"In onPause()");
     }
 
     protected void onStop(){
         super.onStop();
-        Log.i(ACTIVITY_NAME,"In onStop()");
+        Log.i(chatWindow,"In onStop()");
     }
 
     protected void onDestroy(){
         super.onDestroy();
-        Log.i(ACTIVITY_NAME,"In onDestroy()");
+        chatDBHelper.closeDatabase();
+        Log.i(chatWindow,"In onDestroy()");
     }
 
 }
